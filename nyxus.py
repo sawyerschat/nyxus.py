7ccf645f2235939289f89e656f091ca66948db31
sawyerschat <solarmenugtag@gmail.com>
2024-08-27 12:55:11 -0400

    Create nyxus.py
    type shi

---
+++ nyxus.py
@@ -0,0 +1,41 @@
+from colorama import Fore, Style, init
+import os
+import sys
+
+init(autoreset=True)
+
+text = """
+░▒▓███████▓▒░  ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓███████▓▒░ 
+░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░        
+░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░        
+░▒▓█▓▒░░▒▓█▓▒░  ░▒▓██████▓▒░   ░▒▓██████▓▒░  ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓██████▓▒░  
+░▒▓█▓▒░░▒▓█▓▒░    ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░        ░▒▓█▓▒░ 
+░▒▓█▓▒░░▒▓█▓▒░    ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░        ░▒▓█▓▒░ 
+░▒▓█▓▒░░▒▓█▓▒░    ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓██████▓▒░  ░▒▓███████▓▒░  
+"""
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+def display_text():
+    clear_screen()
+    print(Fore.MAGENTA + text)
+    print(Style.RESET_ALL)
+
+def display_options():
+    print(Fore.CYAN + "[1] Test")
+    print(Fore.CYAN + "[2] Test")
+    print(Style.RESET_ALL)
+
+def main():
+    display_text()
+    display_options()
+    choice = input("Select an option: ")
+    if choice == '1':
+        print("You selected option 1.")
+    elif choice == '2':
+        print("You selected option 2.")
+    else:
+        print("Invalid option.")
+
+if __name__ == "__main__":
+    main()
