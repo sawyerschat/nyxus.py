e8c93f618162ccc9a90b7199b29727a2976496cd
sawyerschat <solarmenugtag@gmail.com>
2024-09-09 20:26:08 -0400

    Update nyxus.py

--- nyxus.py
+++ nyxus.py
@@ -14,28 +14,36 @@ text = """
 ░▒▓█▓▒░░▒▓█▓▒░    ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓██████▓▒░  ░▒▓███████▓▒░  
 """
 
+option1 = """
+Nuker >> The Invite Code for the nuker is https://discord.com/oauth2/authorize?client_id=1278453727031857204&permissions=8&integration_type=0&scope=bot, Sometimes, The servers will be down, So you may have to host it on your machine, The code for the nuker will be avaliable on github at 
+"""
+
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 def display_text():
     clear_screen()
     print(Fore.MAGENTA + text)
     print(Style.RESET_ALL)
-
 def display_options():
-    print(Fore.CYAN + "[1] Test")
-    print(Fore.CYAN + "[2] Test")
+    print(Fore.CYAN + "[1] Nuker")
+    print(Fore.CYAN + "[2] Raider")
+    print(Fore.CYAN + "[3] Selfbot")
+    print(Fore.CYAN + "[4] Member Boost")
     print(Style.RESET_ALL)
-
 def main():
     display_text()
     display_options()
-    choice = input("Select an option: ")
+    choice = input("select option: ")
     if choice == '1':
-        print("You selected option 1.")
+        print("nan1")
     elif choice == '2':
-        print("You selected option 2.")
+        print("nan2")
+    elif choice == '3':
+        print("nan3")
+    elif choice == '4':
+        print("nan4")
     else:
-        print("Invalid option.")
+        print("wrong option")
 
 if __name__ == "__main__":
     main()
